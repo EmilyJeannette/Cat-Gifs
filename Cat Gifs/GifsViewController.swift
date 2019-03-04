05f38d82b119d27237799302354fc65c1f43b6df
Emily Kirk <ekirk@emilys-macbook-air-73.local>
2019-03-04 11:51:16 -0600

    Parse JSON data

--- Cat Gifs/GifsViewController.swift
+++ Cat Gifs/GifsViewController.swift
@@ -11,13 +11,41 @@ import UIKit
 class GifsViewController: UITableViewController {
     var data = [[String: String]]()
     let apiKey = "SFiDD63MScry4l67i6NT9Jxqqlc1DDGH"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.title = "Cat Gifs"
         let query = "This call to http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=\(apiKey)"
+        if let url = URL(string: query) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                if json["status"] == "ok" {
+                    parse(json: json)
+                    return
+                }
+            }
+        }
+        loadError()
     }
-
-
+    
+    func parse(json: JSON) {
+        for result in json["data"].arrayValue {
+            let id = result["id"].stringValue
+            let title = result["title"].stringValue
+            let type = result["type"].stringValue
+            let gif = ["id": id, "title": title, "type": type]
+            data.append(gif)
+        }
+        tableView.reloadData()
+    }
+    
+    func loadError() {
+        let alert = UIAlertController(title: "Loading Error",
+                                      message: "There was a problem loading the gif",
+                                      preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil) }
+ 
 }
 
