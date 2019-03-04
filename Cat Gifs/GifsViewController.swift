28dc5f90a69e053d1ab4cc8a55ee4f3efad8879f
Emily Kirk <ekirk@emilys-macbook-air-73.local>
2019-03-04 11:40:19 -0600

    Set up API protocols

--- Cat Gifs/GifsViewController.swift
+++ Cat Gifs/GifsViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class GifsViewController: UITableViewController {
-
+    var data = [[String: String]]()
+    let apiKey = "SFiDD63MScry4l67i6NT9Jxqqlc1DDGH"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.title = "Cat Gifs"
+        let query = "This call to http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=\(apiKey)"
     }
 
 
